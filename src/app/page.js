2e1b2525c816a8056a4ab7b5a191acd3fc4ec5ef
Ryan Whaley <whaleyr@pharmgkb.org>
2024-08-05 13:46:12 -0700

    fix: change front page link

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import styles from "./page.module.css";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -28,7 +27,7 @@ export default function Home() {
 
       <section className="text-center mt-5" id="meeting-2024">
         <h2>ClinPGx Meeting 2024</h2>
-        <p>This meeting took place June 20-21, 2024 at the University of Pennsylvania.<br/><Link href="/meeting/2024/">Presentation videos and slides</Link> are available from the meeting</p>
+        <p>This meeting took place June 20-21, 2024 at the University of Pennsylvania.<br/><a href="/meeting/2024/">Presentation videos and slides</a> are available from the meeting</p>
       </section>
 
       <section className="text-center mt-5">
